94a42c0b900a71b43d61ecebed8e8a2570dab830
Jack Zeng <zeng.zhenhuai@student.greenriver.edu>
2023-01-10 22:58:58 -0500

    home page

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+//  This is my Controller for the hello project
+
+//  Turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+//  Create an instance of the Base class
+$f3 = Base::instance();
+
+//Java equivalent -> Base f3 = new Base();
+
+//Define a default route ("home page for hello project")
+
+$f3 -> route('GET /',function(){
+    echo '<h1>Hello, Fat Free!</h1>';
+});
+
+
+
+
+//   Run Fat-Free
+$f3 ->run();
+
+// Java -> f3.run
